bbd4ecd4550761b29d49475de78eb1af5ea28704
skylerhogan <skyler.hogan@gmail.com>
2021-05-16 10:07:53 -0500

    "Finished Assignment"

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -9,15 +9,17 @@ public class Main {
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
         Job job4 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
-        System.out.println(job4);
-//        ArrayList<Job> jobs = new ArrayList<>();
-//        jobs.add(job1);
-//        jobs.add(job2);
-//        jobs.add(job3);
-//
-//        for (Job job : jobs){
-//            System.out.println(job);
-//        }
+
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+        jobs.add(job4);
+
+        for (Job job : jobs){
+            System.out.println(job);
+        }
     }
 
 }
